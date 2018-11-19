1f223d02cabcae31116588e2887bc30b4988b41e
zmozg <zmozg20@gmail.com>
2018-11-19 17:14:22 +0000

    gunicorn

---
+++ hello.py
@@ -0,0 +1,6 @@
+
+
+def wsgi_application(env, start_response):
+    data = '\n'.join(env['QUERY_STRING'].split('&'))
+    start_response('200 OK', [('Content-Type', 'text/plain')]
+    return data
--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart

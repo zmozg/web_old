7e5cc55e865eef9bd14a4b242d7e05a1d8cb4b21
zmozg <zmozg20@gmail.com>
2019-01-09 09:48:07 +0000

    2 09_01_19

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def Wsgi_application(environ, start_response):
     status = "200 OK"
     headers = [('content-type','text/plain')]
-    body = ['\n'.join(environ['QUERY_STRING'].split('&'))]
+    body = [bytes(i + '\n' , 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response(status, headers)
     return body 

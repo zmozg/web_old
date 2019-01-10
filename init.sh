cb85c95510a40e9fd47f1446d4636a2646da1309
zmozg <zmozg20@gmail.com>
2019-01-10 17:42:25 +0000

    try myvenv

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
+git config --global user.name "zmozg"
+git config --global user.email "zmozg20@gmail.com"
+
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
+source web/myvenv/bin/activate
+gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web hello:Wsgi_application &
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi.application &
 
